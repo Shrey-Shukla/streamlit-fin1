0d024b01e5a27bb67e2dcfdf98dad0d0085a75e9
Shrey <shuklashrey@gmail.com>
2025-05-19 16:42:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ credentials = {
 
 authenticator = stauth.Authenticate(credentials, "portfolio_app", "abcdef", cookie_expiry_days=1)
 
-name, authentication_status, username = authenticator.login("Login", "sidebar")
+name, authentication_status, username = authenticator.login("Login", location="sidebar")
 
 # Simple usage logging function
 def log_user_activity(username, action):
